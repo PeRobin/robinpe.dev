d3b0f10631d31e709a4b822bbf7cbef1049edcfc
PeRobin <robinnibor94@gmail.com>
2025-05-26 22:14:02 +0200

    chore: add social-links.ts constant file

---
+++ src/social-links.ts
@@ -0,0 +1,5 @@
+export const SOCIAL_LINKS = {
+    linkedin: "https://www.linkedin.com/in/pedersenrobin",
+    github:   "https://github.com/perobin",
+    email:    "pedersen.robin@pm.me",
+  } as const;
