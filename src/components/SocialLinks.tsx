efba4767ad09bd5b73349f49f7af649b1921aafa
PeRobin <robinnibor94@gmail.com>
2025-06-03 10:02:20 +0200

    updated social-links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -12,13 +12,13 @@ const SocialLinks: Component<SocialLinksProps> = (props) => {
 
   return (
     <div class="social-links">
-      <a href={props.linkedinUrl} target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+      <a href={props.linkedinUrl} target="_blank" rel="noopener noreferrer" aria-label="Robin Pedersen på LinkedIn">
         <AiFillLinkedin size={iconSize} />
       </a>
-      <a href={props.githubUrl} target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+      <a href={props.githubUrl} target="_blank" rel="noopener noreferrer" aria-label="Robin Pedersen på GitHub">
         <AiFillGithub size={iconSize} />
       </a>
-      <a href={`mailto:${props.email}`} aria-label="Email">
+      <a href={`mailto:${props.email}`} aria-label="Ta kontakt med Robin Pedersen via e-post">
         <AiFillMail size={iconSize} />
       </a>
     </div>
